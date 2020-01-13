3b20552cc8bcbd8a00d3796a071c86f255d41173
Mike Ludemann <mike-ludemann@hotmail.de>
2020-01-13 18:09:31 +0100

    Setup complete - vue-popup

---
+++ src/components/popup.vue
@@ -0,0 +1,183 @@
+<template lang="html">
+	<div class="popup">
+		<div class="popup-text">{{this.defaulttext}}</div>
+		<div :class="['popup-text-' + position]">{{this.popuptext}}</div>
+	</div>
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'popup',
+		props: {
+			defaulttext: {
+				type: String,
+				required: true
+			},
+			popuptext: {
+				type: String,
+				required: true
+			},
+			position: {
+				type: String,
+				required: true
+			}
+		},
+		mounted () {
+			this.insertExternalSource();
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+			insertExternalSource: function () {
+				var popup = document.getElementsByClassName("popup");
+				for (var i = 0; i < popup.length; i++) {
+					popup[i].addEventListener("click", togglePopup(i));
+				}
+				function togglePopup(i) {
+					return function () {
+						if (popup[i].children[1].style.display == "none") {
+							console.log("hidden");
+							popup[i].children[1].style.display == "block";
+						} else {
+							console.log("visible");
+							popup[i].children[1].style.display == "none";
+						}
+					};
+				}
+			}
+		},
+		computed: {
+
+		}
+	}
+</script>
+
+<style scoped lang="css">
+	.popup {
+		position: relative;
+		margin: 100px auto;
+		display: block;
+		width: 125px;
+		height: 100px;
+	}
+
+	.popup .popup-text-top {
+		width: 125px;
+		background-color: #555;
+		color: #fff;
+		text-align: center;
+		border-radius: 5px;
+		padding: 5px;
+		position: absolute;
+		z-index: 1;
+		bottom: 100%;
+		left: 45%;
+		margin-left: -60px;
+		display: none;
+		transition: opacity 0.3s;
+	}
+
+	.popup .popup-text-top::after {
+		content: "";
+		position: absolute;
+		top: 100%;
+		left: 50%;
+		margin-top: 0px;
+		margin-left: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: #555 transparent transparent transparent;
+		transform: rotate(0deg);
+	}
+
+	.popup .popup-text-bottom {
+		width: 125px;
+		background-color: #555;
+		color: #fff;
+		text-align: center;
+		border-radius: 5px;
+		padding: 5px;
+		position: absolute;
+		z-index: 1;
+		top: 170%;
+		left: 45%;
+		margin-left: -60px;
+		display: none;
+		transition: opacity 0.3s;
+	}
+
+	.popup .popup-text-bottom::after {
+		content: "";
+		position: absolute;
+		bottom: 100%;
+		left: 50%;
+		margin-top: 0px;
+		margin-left: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: #555 transparent transparent transparent;
+		transform: rotate(180deg);
+	}
+
+	.popup .popup-text-left {
+		width: 125px;
+		background-color: #555;
+		color: #fff;
+		text-align: center;
+		border-radius: 5px;
+		padding: 5px;
+		position: absolute;
+		z-index: 1;
+		top: 0%;
+		left: -70%;
+		margin-left: -60px;
+		display: none;
+		transition: opacity 0.3s;
+	}
+
+	.popup .popup-text-left::after {
+		content: "";
+		position: absolute;
+		top: 50%;
+		right: -3.5%;
+		margin-top: -5px;
+		margin-right: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: #555 transparent transparent transparent;
+		transform: rotate(-90deg);
+	}
+
+	.popup .popup-text-right {
+		width: 125px;
+		background-color: #555;
+		color: #fff;
+		text-align: center;
+		border-radius: 5px;
+		padding: 5px;
+		position: absolute;
+		z-index: 1;
+		top: 0%;
+		right: -110%;
+		margin-left: -60px;
+		display: none;
+		transition: opacity 0.3s;
+	}
+
+	.popup .popup-text-right::after {
+		content: "";
+		position: absolute;
+		top: 50%;
+		left: -7.5%;
+		margin-top: -5px;
+		margin-right: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: #555 transparent transparent transparent;
+		transform: rotate(90deg);
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<popup defaulttext="Hi" popuptext="I am here!" position="bottom"/>
+		<popup defaulttext="Hi" popuptext="I am here!" position="right"/>
+		<popup defaulttext="Hi" popuptext="I am here!" position="left"/>
+		<popup defaulttext="Hi" popuptext="I am here!" position="top"/>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import popup from '@/components/popup.vue'
+
+export default {
+	name: 'home',
+	components: {
+		HelloWorld,
+		popup
+	}
+}
+</script>
